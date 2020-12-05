87bab477821a342e72abf998d7867505d46e34bb
exit5710 <exit5710@gmail.com>
2020-12-05 22:59:41 +0900

    arrays

--- MDN/arrays/arrays.js
+++ MDN/arrays/arrays.js
@@ -5,4 +5,16 @@
 
 	let fruit = fruits[0];
 	console.log(fruit);
+
+	// forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
+	// value : T (요소(element) : 타입(type))
+	// what is 'thisArg?'?
+	// void (리턴 없음)
+	fruits.forEach(function (item, index) {
+		console.log(item, index);
+	});
+
+	console.log('----------------------------------------');
+
+	fruits.forEach((item, index) => console.log(item, index));
 }
