e3b379fa3a6d80cca4d6c9e4e724a33a2aedc457
exit5710 <exit5710@gmail.com>
2020-12-21 23:18:37 +0900

    insideJS

--- insideJS/chapter04.js
+++ insideJS/chapter04.js
@@ -161,6 +161,25 @@ const fn_newLine = function () {
 {
 	console.log(fn_newLine() + ' -----------------------------------');
 	// 13
+	function func0() {
+	}
+
+	function func1(x) {
+		return x;
+	}
+
+	function func2(x, y) {
+		return x + y;
+	}
+
+	function func3(x, y, z) {
+		return x + y + z;
+	}
+
+	console.log('func0.length - ' + func0.length);
+	console.log('func1.length - ' + func1.length);
+	console.log('func2.length - ' + func2.length);
+	console.log('func3.length - ' + func3.length);
 }
 {
 	console.log(fn_newLine() + ' -----------------------------------');
