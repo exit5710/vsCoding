030300c6c5d6a0263a2d5d721ee807babdf37637
exit5710 <exit5710@gmail.com>
2021-01-02 22:14:16 +0900

    insideJS

--- insideJS/chapter05.js
+++ insideJS/chapter05.js
@@ -156,3 +156,19 @@ const fn_newLine = function () {
 	let inner = outerFunc();
 	inner();
 }
+{
+	console.log(fn_newLine() + ' -----------------------------------');
+	// 8
+	function outerFunc(arg1, arg2) {
+		let local = 8;
+
+		function innerFunc(innerArg) {
+			console.log((arg1 + arg2) / (innerArg + local));
+		}
+
+		return innerFunc;
+	}
+
+	let exam1 = outerFunc(2, 4);
+	exam1(2);
+}
