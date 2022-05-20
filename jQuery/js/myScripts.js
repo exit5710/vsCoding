287a995d33b6f150987d66b35e09059e4dd8425c
exit5710 <exit5710@gmail.com>
2022-05-20 23:14:55 +0900

    jQuery customScript

--- jQuery/js/myScripts.js
+++ jQuery/js/myScripts.js
@@ -1,5 +1,47 @@
-function getRandom(number) {
+// comon
+let getRandom = function (number) {
 	let randomNumber = Math.floor(Math.random() * number);
 
 	return randomNumber;
 };
+
+// ourMenu
+let v = false;
+let fish;
+let meat;
+
+let vegOn = function () {
+	if (v == false) {
+		v = true;
+		fish = $("li.fish").parent().parent().detach();
+
+		$(".hamburger").replaceWith("<li class='portobello'><em>portobello Mushroom</em></li>");
+
+		$(".meat").after("<li class='tofu'>Tofu</li>");
+		meat = $(".meat").detach();
+
+		$(".tofu").parent().parent().addClass("veg_leaf");
+	}
+};
+
+let restoreMe = function () {
+	if (v == true) {
+		v = false;
+
+		$(".menu_entrees li").first().before(fish);
+
+		$(".portobello").replaceWith("<li class='hamburger'>hamburger</li>");
+
+		$(".tofu").parent().parent().removeClass("veg_leaf");
+		$(".tofu").each(function (index) {
+			$(this).replaceWith(meat[index]);
+		});
+	}
+};
+
+$(document).ready(function () {
+
+	// ourMenu
+	$("#vegOn").click(vegOn);
+	$("#restoreMe").click(restoreMe);
+});;
