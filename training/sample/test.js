d6b00cb6bdf7e5c31ab3787434fd19dc77646b34
exit5710 <exit5710@gmail.com>
2022-11-14 00:18:15 +0900

    training

---
+++ training/sample/test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+document.body.style.margin = 0;
+document.body.style.width = '100vw';
+document.body.style.height = '100vh';
+document.body.style.backgroundColor = '#252525';
+
+{
+	function marry(man, woman) {
+		woman.husband = man;
+		man.wife = woman;
+
+		return {
+			father: man,
+			mother: woman
+		};
+	}
+
+	let family = marry({ name: "John" }, { name: "Ann" });
+
+	console.log(family.mother);
+
+	const call = function (message, callBack) {
+		console.log(message);
+
+		for (let i = 0; i <= 55555; i++) {
+			for (let j = 0; j <= 55555; j++) {
+			}
+		}
+
+		if (typeof callBack === 'function') {
+			callBack();
+		}
+	};
+
+	const back = function () {
+		console.log('back');
+	};
+
+	/*
+	call('call', function () {
+		call('hi', function () {
+			call('hello', function () {
+				call('hell', function () {
+					console.log('callBack hell~');
+				});
+			});
+		});
+	});
+	*/
+
+	//call('call', back);
+}
