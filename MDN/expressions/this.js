ebea87009a3fc9a2e95182a33359f24f08c23a7f
exit5710 <exit5710@gmail.com>
2023-07-15 11:22:24 +0900

    MDN expressions

--- MDN/expressions/this.js
+++ MDN/expressions/this.js
@@ -1,40 +1,29 @@
 'use strict';
 
-document.body.style.margin = 0;
-document.body.style.width = '100vw';
-document.body.style.height = '100vh';
-document.body.style.backgroundColor = '#202124';
-
 // this는 호출하는 방법에 의해 결정, 호출한놈(객체) === this
 
-// someOne에 의해 호출되었기 때문에 this는 someOne이 된다. 때문에 name값과 fn의값이 출력된다.
+// someOne에 의해 호출되었기 때문에 this는 someOne이 된다. 때문에 name, value(001), fn의값이 출력된다.
 let someOne = {
 	name: 'kiYoung',
+	value: '001',
 	fn: function () {
-		console.log('someOne', this);
+		console.log('someOne', this.value, this);
 	},
 };
 someOne.fn();
 
-// 버튼을 클릭하여 soneOne.fn이 호출되었기 때문에 this는 버튼이 된다.
+// 버튼을 클릭하여 soneOne.fn이 호출되었기 때문에 this는 버튼이 된다. (버튼의 value값인 5710 출력)
 let btn = document.getElementById('btn');
 btn.style.cursor = "pointer";
-
 btn.addEventListener('click', someOne.fn);
 
-let something = {
-	name: "kiYoung",
-	call: function () {
-		return this.name;
-	}
-};
-
-console.log(something.call());
-
+// calculator
 let calculator = {
 	read: function () {
-		this.a = Number(prompt("첫번째 값 : ", 0));
-		this.b = Number(prompt("두번째 값 : ", 0));
+		// this.a = Number(prompt("첫번째 값 : ", 0));
+		// this.b = Number(prompt("두번째 값 : ", 0));
+		this.a = 2;
+		this.b = 3;
 	},
 	sum: function () {
 		return this.a + this.b;
@@ -44,7 +33,7 @@ let calculator = {
 	}
 };
 
-//calculator.read();
+calculator.read();
 console.log(calculator.sum());
 console.log(calculator.mul());
 
