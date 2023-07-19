56cd7fad46beded46842eeb725f545758e2de0ca
exit5710 <exit5710@gmail.com>
2023-07-20 06:18:20 +0900

    MDN promise

--- MDN/promise/promise.js
+++ MDN/promise/promise.js
@@ -1,13 +1,9 @@
 'use strict';
 
-document.body.style.margin = 0;
-document.body.style.width = '100vw';
-document.body.style.height = '100vh';
-document.body.style.backgroundColor = '#252525';
-
 {
 	const getData = function () {
 		console.log('promise start');
+
 		return new Promise(function (resolve, reject) {
 			setTimeout(function () {
 				resolve('then...');
@@ -15,23 +11,17 @@ document.body.style.backgroundColor = '#252525';
 		});
 	};
 
-	getData().then(function (value) {
-		return new Promise(function (resolve, reject) {
-			setTimeout(function () {
-				resolve(value + 'what!?');
-			}, 2000);
+	getData()
+		.then(function (value) {
+			return new Promise(function (resolve, reject) {
+				setTimeout(function () {
+					resolve(value + 'what!?');
+				}, 2000);
+			});
+		})
+		.then(function (value) {
+			console.log(value);
 		});
-	}).then(function (value) {
-		console.log(value);
-	});
 
 	console.log('hello~');
-
-	const printName = function () {
-		return new Promise(function (resolve, reject) {
-			setTimeout(function () {
-				resolve('kiYoung');
-			}, 2000);
-		});
-	};
 }
