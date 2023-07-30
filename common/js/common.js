74083e5490f02193f737735dae8eb3cca2ded10e
exit5710 <exit5710@gmail.com>
2023-07-30 19:43:24 +0900

    common

--- common/js/common.js
+++ common/js/common.js
@@ -1,6 +1,12 @@
 'use strict';
 
-document.body.style.margin = 0;
-document.body.style.width = '100vw';
-document.body.style.height = '100vh';
-document.body.style.backgroundColor = '#202124';
+const fn_init = function () {
+	document.body.style.margin = 0;
+	document.body.style.width = '100vw';
+	document.body.style.height = '100vh';
+	document.body.style.backgroundColor = '#202124';
+};
+
+const fn_newLine = function (str) {
+	console.log('\n--- ' + str + ' ----------------------------------------');
+};
