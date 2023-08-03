540acc33b7d017cd30b3f89479913baa2ef7e590
exit5710 <exit5710@gmail.com>
2023-08-03 22:59:28 +0900

    javascript map

--- training/script/map/map.js
+++ training/script/map/map.js
@@ -1,24 +1,77 @@
 'use strict';
 
-/*
-(method) Array<number>.map<boolean>(callbackfn: (value: number, index: number, array: number[]) => boolean, thisArg?: any): boolean[]
-Calls a defined callback function on each element of an array, and returns an array that contains the results.
-
-@param callbackfn — A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
-
-@param thisArg — An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
-*/
 {
-	fn_newLine('map start');
+	fn_newLine('Map');
+	const map = new Map();
 
-	let numbers = ['1', '2', '3'];
+	map.set('1', 'str'); // 문자형 키
+	map.set(1, 'num'); // 숫자형 키
+	map.set(true, 'bool'); // 불린형 키
+	//map.set('1', 'string').set(1, 'number').set(true, 'bool');
 
-	let fn_print = function (elemnt) {
-		console.log(elemnt);
+	const person = { name: 'Jone', age: 23 };
+	map.set(person, person.name);
 
-		return elemnt.length;
+	console.log(map.get(1));
+	console.log(map.get(person), map.size);
+
+	let recipeMap = new Map([
+		['cucumber', 500],
+		['tomatoes', 350],
+		['onion', 50],
+	]);
+
+	recipeMap.forEach(function (element, key) {
+		console.log(element, key);
+	});
+
+	fn_newLine('object -> map');
+	let obj = { name: 'tom', age: 23, address: 'usa' };
+	let entrieMap = new Map(Object.entries(obj));
+
+	console.log(entrieMap.get('name'));
+
+	fn_newLine('map -> object');
+	let fromEntrieObj = Object.fromEntries(entrieMap);
+
+	console.log(fromEntrieObj.age);
+
+	fn_newLine('Set');
+	let set = new Set();
+
+	let john = { name: 'John' };
+	let pete = { name: 'Pete' };
+	let mary = { name: 'Mary' };
+
+	set.add(john);
+	set.add(pete);
+	set.add(mary);
+	set.add(john);
+	set.add(mary);
+
+	set.forEach(function (element) {
+		console.log(element.name);
+	});
+
+	fn_newLine('array duplicate');
+	let numbers = [1, 2, 3, 1, 5, 6, 5, 9];
+	let numberSet = new Set(numbers);
+
+	numbers = [];
+
+	numberSet.forEach(function (elements) {
+		numbers.push(elements);
+	});
+
+	console.log(numbers);
+
+	fn_newLine('ex1');
+	// Hare, Krishna, :-O 만 출력
+	let fn_unique = function (element) {
+		return Array.from(new Set(element));
 	};
 
-	let result = numbers.map(fn_print);
-	console.log(result);
+	let values = ['Hare', 'Krishna', 'Hare', 'Krishna', 'Krishna', 'Krishna', 'Hare', 'Hare', ':-O'];
+
+	console.log(fn_unique(values));
 }
