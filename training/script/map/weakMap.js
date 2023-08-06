af2c1b59168a1cdc3fd7552c879ac2aca6560ddc
exit5710 <exit5710@gmail.com>
2023-08-06 19:18:38 +0900

    script weakMap

--- training/script/map/weakMap.js
+++ training/script/map/weakMap.js
@@ -24,5 +24,42 @@
 
 	jim = null;
 
-	console.log(weakMap.get(jim));
+	console.log(weakMap);
+
+	fn_newLine('visitsCount');
+	let visitsCountMap = new WeakMap();
+
+	let fn_countUser = function (user) {
+		let count = visitsCountMap.get(user) || 0;
+		visitsCountMap.set(user, count + 1);
+	};
+
+	let tom = { name: 'tom' };
+	fn_countUser(tom);
+
+	tom = null;
+
+	console.log(visitsCountMap);
+
+	fn_newLine('ex1'); // '읽음'상태인 메시지 저장하기
+
+	// 메세지
+	let messages = [
+		{ text: 'Hello', from: 'John' },
+		{ text: 'How goes?', from: 'John' },
+		{ text: 'See you soon', from: 'Alice' },
+	];
+
+	// 읽은 메세지
+	let readMessages = new WeakSet();
+
+	// 2개의 메세지를 읽음
+	readMessages.add(messages[0]);
+	readMessages.add(messages[1]);
+
+	// 메세지 배열에서 읽은 메세지 삭제
+	messages.splice(0, 2);
+
+	console.log('messages : ', messages);
+	console.log('readMessages: ', readMessages);
 }
