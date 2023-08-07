020ba8a9f5fb7220c399cb4e72013c08b24da7bb
exit5710 <exit5710@gmail.com>
2023-08-07 21:35:25 +0900

    MDN callBack

--- MDN/function/callBack.js
+++ MDN/function/callBack.js
@@ -3,6 +3,7 @@
 // callBack함수 (함수를 인자로 콜백함수를 넘겨 특정시점에 실행)
 // https://yubylab.tistory.com/entry/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EC%9D%98-%EC%BD%9C%EB%B0%B1%ED%95%A8%EC%88%98-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0?category=575633
 {
+	fn_newLine('');
 	let allUserData = [];
 
 	let logStuff = function (userData) {
@@ -38,7 +39,7 @@
 }
 
 {
-	console.log('---------------------------------------------------------------------');
+	fn_newLine('');
 	// getUserInput함수에서 callBack(clientData.setUserName)을 호출하지만 setUserName에서는 this가 undefined가 되어 fullName에 Barack Obama를 세팅하지 못한다.
 	// function안의 this는 undefined // -ex1, 엄격모드가 아니라면 window객체가 된다.
 	// this는 호출하는 방법에 의해 결정되는데 함수에서 호출되기 때문에 undefined가된다.
@@ -76,7 +77,7 @@
 }
 
 {
-	console.log('---------------------------------------------------------------------');
+	fn_newLine('');
 
 	let clientData = {
 		id: '094545',
