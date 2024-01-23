6cb9d431dee81db8070ea53c2a7c298e892f07f9
exit5710 <exit5710@gmail.com>
2024-01-23 22:37:58 +0900

    MDN arrays reduce

--- MDN/arrays/reduce.js
+++ MDN/arrays/reduce.js
@@ -2,10 +2,10 @@
 
 // The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order,
 // passing in the return value from the calculation on the preceding element.
-// 배열 인스턴스의 reduce 메소드는    실행한다 사용자 제공 "reducer" 콜백 함수      배열의 각 요소를
+// 배열 인스턴스의 reduce() 메소드는 배열의 각 요소에 사용자가 제공하는 "reducer" 콜백 함수를 적절히 실행하고, 이전요소에 계산된 리턴값을 전단한다.
 
 // The final result of running the reducer across all elements of the array is a single value.
-//
+// 배열의 모든 요소에 실행한 최종 결과는 단일 값이다.
 
 // The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place.
 //
