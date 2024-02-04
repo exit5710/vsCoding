fe367906351ecc0ce8feca8ee00e864fb5d48ef7
exit5710 <exit5710@gmail.com>
2024-02-04 21:32:54 +0900

    MDN object assign

---
+++ MDN/object/assign.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+	fn_newLine('object');
+}
+
+{
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+	fn_newLine('object assign');
+}
