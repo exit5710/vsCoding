e18fdb02db0b4d01edf916ca4d0128275214cc14
exit5710 <exit5710@gmail.com>
2024-07-01 23:42:55 +0900

    MDN closure

--- MDN/function/closure.js
+++ MDN/function/closure.js
@@ -20,25 +20,44 @@
 
 	fn_newLine();
 
-	let phrase = 'hello';
+	let sumRemainders = function () {
+		let sum = 0;
 
-	function say(name) {
-		console.log(`${phrase}, ${name}`);
-	}
+		return function (num) {
+			let remainders = num % 2;
 
-	say('tom');
+			if (remainders === 1) {
+				sum++;
+			}
+
+			return sum;
+		};
+	};
+
+	let remainders = sumRemainders();
+
+	console.log(remainders(1));
+	console.log(remainders(1));
+	console.log(remainders(2));
+	console.log(remainders(2));
+	console.log(remainders(1));
 
 	fn_newLine();
 
 	function f() {
-		let value = 123;
+		let value = Math.random();
 
 		return function () {
-			alert(value);
+			return value;
 		};
 	}
 
-	let g = f();
+	let arr = [f(), f(), f()];
+	console.log(arr[0]());
+	console.log(arr[1]());
+	console.log(arr[2]());
+
+	fn_newLine();
 
 	//fn_terminated();
 }
