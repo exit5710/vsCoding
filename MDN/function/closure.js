3cbd8a52393397a40cbe5c425a37b31c262f1798
exit5710 <exit5710@gmail.com>
2024-07-31 00:28:05 +0900

    closure

--- MDN/function/closure.js
+++ MDN/function/closure.js
@@ -59,5 +59,69 @@
 
 	fn_newLine();
 
+	let result = calculator.add(5);
+	result = calculator.subtract(2);
+	console.log(result);
+
+	fn_newLine();
+
+	let array = [1, 2, 3, 4, 5, 6, 7];
+
+	/*
+	let inBetween = array.filter(function (element) {
+		return element >= 3 && element <= 6;
+	});
+	console.log(inBetween);
+	*/
+
+	let inBetween = function (from, to) {
+		return function (element) {
+			return element >= from && element <= to;
+		};
+	};
+	console.log(array.filter(inBetween(3, 6)));
+
+	/*
+	let inArray = array.filter(function (element) {
+		return [2, 5, 9].includes(element);
+	});
+	console.log(inArray);
+
+	[2, 5, 9]를 파라미터로 받는 function (array)
+	filter에서 element를 사용하는 function (element)
+	파라미터의 includes(element)
+	*/
+
+	let inArray = function (array) {
+		return function (element) {
+			return array.includes(element);
+		};
+	};
+	console.log(array.filter(inArray([2, 5, 7, 9])));
+
+	fn_newLine();
+
+	let users = [
+		{ name: 'John', age: 20, surname: 'Johnson' },
+		{ name: 'Pete', age: 18, surname: 'Peterson' },
+		{ name: 'Ann', age: 19, surname: 'Hathaway' },
+	];
+	//users.sort((a, b) => (a.name > b.name ? 1 : -1));
+	/*
+	users.sort(function (a, b) {
+		return a.name > b.name ? 1 : -1;
+	});
+	*/
+
+	let byField = function (fieldName) {
+		return function (a, b) {
+			return a[fieldName] > b[fieldName] ? 1 : -1;
+		};
+	};
+
+	users.sort(byField('age'));
+
+	console.log(users);
+
 	//fn_terminated();
 }
