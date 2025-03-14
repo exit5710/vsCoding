02aa5a70f1539fb3a1e01ad556adeaaed0455675
exit5710 <exit5710@gmail.com>
2025-03-15 01:53:45 +0900

    includes

---
+++ MDN/arrays/includes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// The includes() method of Array instances determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+//
+{
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+	fn_newLine('array includes');
+
+	let array = [1, 2, 3, 4, 5, 6, 7];
+	console.log(array.includes(2));
+}
