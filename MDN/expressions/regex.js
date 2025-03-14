62ab682fbb57438737c84693d7e7ed186653e530
exit5710 <exit5710@gmail.com>
2025-03-15 01:54:00 +0900

    regex

---
+++ MDN/expressions/regex.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+	// https://
+	fn_newLine('regex');
+
+	let say = 'my name is tom';
+	let replaced = say.replaceAll(/[^\s]/g, '-');
+	console.log(replaced);
+
+	//fn_terminated();
+}
