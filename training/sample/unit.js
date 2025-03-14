60687fbdfcc73b7757bf3a0e910ae8e1e6a754a5
exit5710 <exit5710@gmail.com>
2025-03-15 01:52:45 +0900

    unit

---
+++ training/sample/unit.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+	// unit test
+	fn_newLine('unit test');
+
+	function askPassword(ok, fail) {
+		let password = prompt('비밀번호를 입력해주세요.', '');
+		if (password == 'rockstar') ok();
+		else fail();
+	}
+
+	let user = {
+		name: 'John',
+
+		login(result) {
+			alert(this.name + (result ? ' 로그인 성공' : ' 로그인 실패'));
+		},
+	};
+
+	askPassword(user.login.bind(user, true), user.login.bind(user, false));
+
+	//fn_terminated();
+}
