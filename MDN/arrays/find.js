ea265c970395f63c1a379ec13a1060fde414470a
exit5710 <exit5710@gmail.com>
2026-03-03 00:57:04 +0900

    find

---
+++ MDN/arrays/find.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// The find() method of Array instances returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+
+// If you need the index of the found element in the array, use findIndex().
+// If you need to find the index of a value, use indexOf(). (It's similar to findIndex(), but checks each element for equality with the value instead of using a testing function.)
+// If you need to find if a value exists in an array, use includes(). Again, it checks each element for equality with the value instead of using a testing function.
+// If you need to find if any element satisfies the provided testing function, use some().
+// If you need to find all elements that satisfy the provided testing function, use filter().
+
+{
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+	fn_newLine('find');
+}
