0ad1c0aaa88b3627baa62f843fbe05a8bc896aa6
exit5710 <exit5710@gmail.com>
2026-03-03 01:05:09 +0900

    bind

--- MDN/function/bind.js
+++ MDN/function/bind.js
@@ -8,13 +8,9 @@
 	const user = {
 		firstName: 'jim',
 		sayHi: function () {
-			console.log(`Hello, ${this.firstName}!`);
+			console.log('Hello, ' + this.firstName);
 		},
 	};
-
-	// setTimeout(function () {
-	// 	user.sayHi();
-	// }, 1000);
 
 	let func = function () {
 		this.sayHi();
@@ -22,6 +18,25 @@
 
 	let bindFunc = func.bind(user);
 	bindFunc();
+
+	/*
+	function askPassword(ok, fail) {
+		let password = prompt('비밀번호를 입력해주세요.', '');
+
+		if (password == 'rockstar') ok();
+		else fail();
+	}
+
+	let user = {
+		name: 'John',
+
+		login(result) {
+			console.log(this.name + (result ? ' 로그인 성공' : ' 로그인 실패'));
+		},
+	};
+
+	askPassword(user.login.bind(user, true), user.login.bind(user, false));
+	*/
 
 	//fn_terminated();
 }
