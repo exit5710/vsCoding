3150cf21dd0139c84c4cb0cee799e9b91588dd36
exit5710 <exit5710@gmail.com>
2026-03-03 01:04:09 +0900

    common

--- common/js/common.js
+++ common/js/common.js
@@ -2,8 +2,8 @@
 
 const fn_init = function () {
 	document.body.style.margin = 0;
-	document.body.style.width = '100vw';
-	document.body.style.height = '100vh';
+	document.body.style.width = 'calc(100vw - 22px)';
+	document.body.style.height = 'calc(100vh - 22px)';
 	document.body.style.backgroundColor = '#202124';
 };
 
@@ -17,6 +17,40 @@ const fn_newLine = function (...args) {
 
 const fn_addZero = function (number, addNumber = 2) {
 	return number.toString().padStart(addNumber, '0');
+};
+
+const fn_submit = async function (url, jsonData, callBack) {
+	try {
+		const response = await fetch(url, {
+			// method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			// body: JSON.stringify(jsonData),
+		});
+
+		const contentType = response.headers.get('content-type');
+
+		if (!contentType) {
+			console.error('No content-type in response');
+			return false;
+		}
+
+		// 일반 JSON 응답 처리
+		if (contentType.includes('application/json')) {
+			const data = await response.json();
+
+			if (typeof callBack === 'function') {
+				callBack(data);
+			}
+
+			return false;
+		}
+
+		console.warn('지원하지 않는 Content-Type:', contentType);
+	} catch (error) {
+		console.error('Error:', error);
+	}
 };
 
 const fn_terminated = function () {
